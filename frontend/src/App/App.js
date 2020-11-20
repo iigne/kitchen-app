0b59e0a62969fe93b7791337752f6d5418a9dd6f
Igne Stalnionyte <igne.stal@gmail.com>
2020-11-20 23:09:41 +0000

    add base for login

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -25,7 +25,6 @@ class App extends Component {
     }
 
     render() {
-
         return(
             <div className="App">
                 <Router>
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,9 +1,64 @@
-import {Component} from "react";
+import React, {Component} from "react";
+import {Alert, Button, Form} from "react-bootstrap";
+import axios from "axios";
 
 class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            success: null
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        let toSave = {
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password
+        }
+        axios.post('/user', toSave, {
+            "Content-Type":"application/json"
+        })
+            .then(res => this.setState({success: true})
+            )
+            .catch(error => this.setState({success: false}));
+    }
+
     render() {
+        let error
+        if(this.state.success!= null && !this.state.success) {
+            error = <Alert variant={"danger"}>Login failed. Please check your details again</Alert>
+        }
         return(
-            <h2>login</h2>
+            <div className="Login">
+                {error}
+                <h2>Login</h2>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="formUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control name="username" type="text" value={this.state.username} onChange={this.handleChange}/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control name="password" type="password" value={this.state.password} onChange={this.handleChange}/>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         )
     }
 }
