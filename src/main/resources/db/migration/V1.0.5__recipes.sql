413b366ee13ee948cb93387f899cc9aa7250bf48
Igne Stalnionyte <igne.stal@gmail.com>
2021-02-10 17:53:17 +0000

    add recipes tables

---
+++ src/main/resources/db/migration/V1.0.5__recipes.sql
@@ -0,0 +1,25 @@
+CREATE TABLE recipe (
+    id int primary key identity (1,1) not null,
+    title varchar(255) not null,
+    image_link varchar(255),
+    method text not null
+);
+
+CREATE TABLE recipe_ingredient (
+     recipe_id int not null,
+     ingredient_id int not null,
+     metric_quantity float not null,
+     custom_measurement_id int,
+     constraint FK_recipe_ingredient_recipe foreign key (recipe_id) references recipe(id),
+     constraint FK_recipe_ingredient_custom_measurement foreign key (custom_measurement_id) references measurement(id),
+     constraint FK_recipe_ingredient_ingredient foreign key (ingredient_id) references ingredient(id),
+     constraint PK_recipe_ingredient primary key (recipe_id, ingredient_id)
+);
+
+CREATE TABLE user_recipe (
+    user_id int not null,
+    recipe_id int not null,
+    constraint FK_user_recipe_user foreign key (user_id) references [user](id),
+    constraint FK_user_recipe_recipe foreign key (recipe_id) references recipe(id)
+)
+
