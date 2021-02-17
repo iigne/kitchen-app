ce46b260eb5ba046a2c793e941707948eec0f07e
Igne Stalnionyte <igne.stal@gmail.com>
2021-02-17 21:55:51 +0000

    implement base for recipe card

---
+++ frontend/src/components/Recipes/RecipeCardIngredient.js
@@ -0,0 +1,30 @@
+import {Col, ListGroupItem, Row} from "react-bootstrap";
+import React from "react";
+
+class RecipeCardIngredient extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.name,
+            recipeQuantity: props.recipeQuantity,
+            ownedQuantity: props.ownedQuantity,
+            measurement: props.measurement
+        }
+    }
+
+    render() {
+        const hasEnoughIngredient = this.state.recipeQuantity < this.state.ownedQuantity;
+        const itemVariant = hasEnoughIngredient ? "success" : "danger";
+        return(
+            <ListGroupItem variant={itemVariant}>
+                <Row>
+                    <Col xs={3}>{this.state.name}</Col>
+                    <Col xs={3}>{this.state.recipeQuantity} {this.state.measurement}</Col>
+                    {/*TODO implement*/}
+                    <Col>(you have 0 {this.state.measurement})</Col>
+                </Row>
+
+            </ListGroupItem>
+        )
+    }
+} export default RecipeCardIngredient;
