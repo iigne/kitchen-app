0e978112d51b87451b5ba6a08894a5343fe6a357
Igne Stalnionyte <igne.stal@gmail.com>
2021-02-20 21:59:57 +0000

    fix build, update packages

--- frontend/src/components/UserStock/AddMeasurement.js
+++ frontend/src/components/UserStock/AddMeasurement.js
@@ -1,5 +1,5 @@
 import React from "react";
-import  {Row, Button, Col, Form, FormGroup, FormLabel, InputGroup} from "react-bootstrap";
+import  {Button, Col, Form, FormGroup, FormLabel, InputGroup} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlus, faTrash} from "@fortawesome/free-solid-svg-icons";
 
