b678f1433b11caedd2fb2e3c3b2992e362b868fb
Igne Stalnionyte <igne.stal@gmail.com>
2021-02-20 21:30:30 +0000

    move local env setup to one script

--- database_init.sh
+++ database_init.sh
@@ -2,4 +2,10 @@ docker kill sql-server-db
 docker rm sql-server-db
 docker run --name sql-server-db -d -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=VZa4uVjFnRXnF62bQ' -p 1433:1433 mcr.microsoft.com/mssql/server:2019-latest
 
-mssql -u sa -p VZa4uVjFnRXnF62bQ
+echo waiting for database to start up...
+sleep 10
+
+mssql -u sa -p VZa4uVjFnRXnF62bQ -q "create database kitchenapp;"
+mssql -u sa -p VZa4uVjFnRXnF62bQ -d kitchenapp -q "create login kitchenapp with password='VZa4u&VjFnRXn&F62bQ';create user kitchenapp for login kitchenapp;exec sp_addrolemember 'db_owner', kitchenapp;"
+
+echo SETUP COMPLETE!
--- database_script.sql
+++
@@ -1,5 +0,0 @@
-create database kitchenapp;
-use kitchenapp;
-create login kitchenapp with password='VZa4u&VjFnRXn&!F62bQ';
-create user kitchenapp for login kitchenapp;
-exec sp_addrolemember 'db_owner', kitchenapp;
