55e601c17901c93e603e628d3e33d7d2679cd611
Igne Stalnionyte <igne.stal@gmail.com>
2021-03-22 12:20:37 +0000

    add missing icon

--- frontend/src/components/Ingredient/icons.js
+++ frontend/src/components/Ingredient/icons.js
@@ -1,10 +1,11 @@
-import {faAppleAlt, faBox, faLeaf, faPepperHot, faSnowflake} from "@fortawesome/free-solid-svg-icons";
+import {faAppleAlt, faBox, faBreadSlice, faLeaf, faPepperHot, faSnowflake} from "@fortawesome/free-solid-svg-icons";
 
 export const icons = new Map([
     ['Vegetable', { icon: faLeaf, color: "#66B447"}],
     ["Fruit", {icon: faAppleAlt, color:"#E9692C"}],
     ["Cupboard",{icon: faBox, color:"#d4bc89"}],
     ["Fridge", {icon: faSnowflake, color:"#a0cbf8"}],
+    ["Bread", {icon: faBreadSlice, color:"#EEC07B"}],
     ["Freezer", {icon: faSnowflake, color:"#1530b1"}],
     ["Spice", {icon: faPepperHot, color:"#000000"}],
     ["Other", {icon: null}]
